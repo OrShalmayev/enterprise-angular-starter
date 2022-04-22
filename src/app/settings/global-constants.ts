7cc5cd90d0cfb7e53be13a1fe5676905bfdd28f0
orshalmayev <orsh@ofakim-group.com>
2022-04-22 18:05:33 +0300

    added global constants

---
+++ src/app/settings/global-constants.ts
@@ -0,0 +1,7 @@
+export class GlobalConstants {
+    public static readonly APP_NAME: string = 'Angular-Material-App';
+    public static readonly APP_VERSION: string = '1.0.0';
+    public static readonly APP_DESCRIPTION: string = 'Angular Material App';
+    public static readonly APP_AUTHOR: string = 'Juan Pablo Vidal';
+    public static readonly APP_AUTHOR_URL: string = '';
+};

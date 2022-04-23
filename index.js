40c0e03d64cb68f95c021a0abe30167fa6bf329d
orshalmayev <orsh@ofakim-group.com>
2022-04-23 18:08:33 +0300

    changed dir

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/orsh-weather'));
+app.use(express.static(__dirname + '/dist/ngrx-best-practice'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/orsh-weather/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ngrx-best-practice/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);

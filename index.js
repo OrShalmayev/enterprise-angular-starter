65c6d7d037d6a906fc021e53b1eb83ee65f3d141
orshalmayev <orsh@ofakim-group.com>
2022-04-23 18:37:55 +0300

    added compression

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const compression = require('compression')
 app.use(express.static(__dirname + '/dist/ngrx-best-practice'));
+app.use(compression())
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/ngrx-best-practice/index.html'));

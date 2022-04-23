55c90bbb20747e03341a73f056de93bb2ad88e8e
orshalmayev <orsh@ofakim-group.com>
2022-04-23 19:02:20 +0300

    s

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('path');
 const app = express();
 const compression = require('compression')
 app.use(express.static(__dirname + '/dist/ngrx-best-practice'));
-app.use(compression())
+app.use(compression({
+    filter: function (req, res) {
+      return (/json|text|javascript|css|font|svg/).test(res.getHeader('Content-Type'));
+    },
+    level: 9
+}));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/ngrx-best-practice/index.html'));

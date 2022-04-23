9336f9997eef0b6d3466ddae0c0943859a4f8595
orshalmayev <orsh@ofakim-group.com>
2022-04-23 17:49:11 +0300

    changed to no lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
 
 const routes: Routes = [
     {
         path: '', 
         pathMatch: 'full', 
-        loadChildren: ()=> import('./app-layout/app-layout.module').then(m=>m.AppLayoutModule),
+        component: AppLayoutComponent,
     },
 
 ];

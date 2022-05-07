f18d746377e8336804dd2d3db88a7278744b4aaa
orshalmayev <orsh@ofakim-group.com>
2022-05-07 12:27:08 +0300

    added index to utility

--- src/app/modules/@core/utility/index.ts
+++ src/app/modules/@core/utility/index.ts
@@ -1,2 +1,6 @@
 export * from './store-actions.helper';
 export * from './validators/password.validator';
+export * from './util';
+export * from './rxjs/convertObservableToBehaviorSubject';
+export * from './rxjs/operators';
+export * from './markFormAs';

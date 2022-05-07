6aabf0af3be0e36df5004c6e65cea1ee59aa7a9c
orshalmayev <orsh@ofakim-group.com>
2022-05-07 12:30:47 +0300

    shared.module.ts

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,21 +1,26 @@
-import {NgModule} from "@angular/core";
+import {ModuleWithProviders, NgModule} from "@angular/core";
 import {RouteLoadingComponent} from './components/route-loading/route-loading.component';
 import {NgLetDirective} from "./directives/";
 
-const exports: any[] = [
+const COMPONENTS: any[] = [
     RouteLoadingComponent,
     NgLetDirective,
 ];
+const DIRECTIVES: any[] = [];
+const PIPES: any[] = [];
+const MODULES: any[] = [];
+const EXPORTS: any[] = [];
 
 @NgModule({
-    declarations: [
-        ...exports,
-    ],
-    imports: [
-    ],
-    providers: [],
-    exports,
+    declarations: [...COMPONENTS, ...DIRECTIVES, ...PIPES],
+    imports: [...MODULES, ...EXPORTS],
+    exports: [...MODULES, ...COMPONENTS, ...PIPES, ...DIRECTIVES, ...EXPORTS],
 })
 export class SharedModule {
+    static forRoot(): ModuleWithProviders<SharedModule> {
+        return {
+            ngModule: SharedModule,
+        };
+    }
 }
 
